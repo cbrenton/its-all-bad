8597f4d510667a8d09d4f93acda01aa7ba8c05de
Chris Brenton <hiring@rubyridge.org>
2026-04-15 23:57:48 -0400

    boing

--- Gun.cs
+++ Gun.cs
@@ -12,13 +12,26 @@ public partial class Gun : RigidBody2D
   private Sprite2D Sprite;
   private AnimationPlayer ShootAnimation;
   private double FireCooldown;
+  private AudioStreamPlayer2D Boing;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+    Boing = GetNodeOrNull<AudioStreamPlayer2D>("BounceStream");
     Sprite = GetNodeOrNull<Sprite2D>("Sprite2D");
     Shooty = GetNodeOrNull<RayCast2D>("Shooty");
     ShootAnimation = GetNodeOrNull<AnimationPlayer>("GunshotAnimation");
+
+    ContactMonitor = true;
+    MaxContactsReported = 4;
+
+    BodyShapeEntered += (a, b, c, d) => {
+      GD.Print($"boom {LinearVelocity.Length()}");
+      if (LinearVelocity.Length() > 50) {
+         Boing.VolumeLinear = LinearVelocity.Length() / 100;
+         Boing.Play();
+      }
+    };
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
