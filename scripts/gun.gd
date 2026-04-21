1a263ff473ca29fd00a86504447b34e0eadd5799
Chris Brenton <hiring@rubyridge.org>
2026-04-21 14:44:34 -0400

    Create scenes and scripts dirs, and reformat all files

---
+++ scripts/gun.gd
@@ -0,0 +1,103 @@
+class_name Gun
+extends RigidBody2D
+
+signal hit_landed(target, is_fatal)
+signal shot
+
+@onready var raycast: RayCast2D = $HitRay2D
+
+# TODO: start false
+var is_loaded: bool = true
+var attack_duration: float = 0.1
+var attack_timer: Timer
+var is_thrown: bool = false
+var attack_rate: float = 0.2
+var holder: Player = null
+
+
+func _ready() -> void:
+    attack_rate = 0.2
+    print("gun ready")
+    self.contact_monitor = true
+    self.max_contacts_reported = 4
+    body_entered.connect(_on_body_entered)
+    self.continuous_cd = RigidBody2D.CCD_MODE_CAST_SHAPE
+
+
+func _on_pickup(player: Player) -> void:
+    self.holder = player
+    move_to.call_deferred(player)
+
+
+# TODO: when weapon is held, disable pickup
+func move_to(player: Player) -> void:
+    self.freeze_mode = RigidBody2D.FREEZE_MODE_KINEMATIC
+    self.freeze = true
+    reparent(player.hold_position)
+    self.position = Vector2.ZERO
+    self.rotation = 0
+
+    self.hit_landed.connect(player.attack)
+
+
+func drop() -> void:
+    # TODO: figure out how to do this with fist
+    self.freeze = false
+    reparent(get_tree().current_scene)
+    self.hit_landed.disconnect(self.holder.attack)
+    self.holder = null
+
+
+func _fire() -> void:
+    if self.is_loaded:
+        if raycast.is_colliding():
+            print("bam!")
+            var other = self.raycast.get_collider() as Player
+            if other:
+                self.hit_landed.emit(other, true)
+        self.is_loaded = false
+        self.shot.emit()
+    else:
+        _throw()
+
+
+func _throw() -> void:
+    var look_dir = sign(self.holder.visuals.scale.x)
+    self.holder.clear_weapon()
+    drop()
+    self.is_thrown = true
+    apply_central_impulse(Vector2(look_dir * 2000, -20))
+    pass
+
+
+func attack() -> void:
+    if self.attack_timer == null:
+        self.attack_timer = Timer.new()
+        self.attack_timer.one_shot = true
+        add_child(self.attack_timer)
+    if self.attack_timer.is_stopped():
+        _fire()
+        self.attack_timer.start(self.attack_rate)
+    pass
+
+
+func _on_body_entered(body: Node) -> void:
+    var env = body as StaticBody2D
+    var player = body as Player
+
+    if env:
+        print("gun hit body is ground or wall")
+        await get_tree().create_timer(0.5).timeout
+        self.is_thrown = false
+    elif player and is_thrown:
+        print("gun hit player")
+        var dir_to_target = sign((player.position - self.position).x)
+        var knockback_force = 200
+        player.receive_knockback(Vector2(dir_to_target * knockback_force, -200))
+    else:
+        print("gun hit %s" % body.name)
+    pass  # Replace with function body.
+
+
+func is_held() -> bool:
+    return self.holder != null
---
+++ scripts/match_controller.gd
@@ -0,0 +1,60 @@
+extends Node2D
+
+@onready var player_scene = preload("res://scenes/player.tscn")
+@onready var gun_scene = preload("res://scenes/gun.tscn")
+@onready var bullet_scene = preload("res://scenes/bullet.tscn")
+
+var rng = RandomNumberGenerator.new()
+
+
+func _ready() -> void:
+    _start_match()
+
+
+func _start_match() -> void:
+    _spawn_players()
+
+    _spawn_gun()
+
+    _spawn_bullet()
+
+
+func _spawn_players() -> void:
+    # TODO: make this use screen space to determine spawn point
+    var player_positions = [Vector2(200, 500), Vector2(800, 500)]
+
+    var player_num = 1
+    for pos in player_positions:
+        # instantiate player
+        breakpoint
+        var player = self.player_scene.instantiate()
+        # set player pos and orientation
+        player.position = pos
+        # TODO: connect player signals
+        add_child(player)
+        player.initialize(player_num)
+        player_num += 1
+        player.die.connect(_check_for_winner)
+
+
+func _spawn_gun():
+    var gun = self.gun_scene.instantiate()
+    gun.position = Vector2(571, 0)
+    gun.rotation = rng.randi_range(0, 360)
+    gun.apply_central_impulse(Vector2(rng.randi_range(50, 100), 20))
+    gun.shot.connect(_spawn_bullet)
+    add_child(gun)
+
+
+func _spawn_bullet():
+    await get_tree().create_timer(2.0).timeout
+
+    var bullet = self.bullet_scene.instantiate()
+    bullet.position = Vector2(571, 0)
+    add_child(bullet)
+
+
+func _check_for_winner(player: Player) -> void:
+    # brief async - delete players, gun, bullet
+    # display win label? emit signal upwards?
+    print("p%d died" % player.player_number)
